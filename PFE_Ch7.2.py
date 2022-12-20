263f00344da966319700c0bb69290e250622bc18
Stephen Baum <stephen_baum@berkeley.edu>
2022-12-19 22:24:46 -0800

    Update PFE_Ch7.2.py

--- PFE_Ch7.2.py
+++ PFE_Ch7.2.py
@@ -5,7 +5,7 @@
 # Write a program that prompts for a file name, 
 # then opens that file and reads through the file, 
 # looking for lines of the form:
- # X-DSPAM-Confidence:    0.8475
+# X-DSPAM-Confidence:    0.8475
 # Count these lines and extract the floating point 
 # values from each of the lines and compute the average 
 # of those values and produce an output as shown below. 
